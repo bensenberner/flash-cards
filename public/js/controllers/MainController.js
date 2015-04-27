a18aca7d9fc66fc80536c543f06fb299ff2e1c04
Ben Lerner <benlerner95@gmail.com>
2015-04-27 11:30:30 -0400

    added border on hover

---
+++ public/js/controllers/MainController.js
@@ -0,0 +1,42 @@
+app.controller('MainController', function ($scope, FlashCardsFactory, ScoreFactory) {
+
+  $scope.categories = [
+    'MongoDB',
+    'Express',
+    'Angular',
+      'Node',
+      ''
+  ];
+
+
+
+  $scope.loaded = false;
+
+  FlashCardsFactory.getFlashCards().then(function (data)
+      {
+        $scope.loaded = true;
+        $scope.flashCards = data;
+      });
+
+
+  $scope.getCategoryCards = function (category)
+  {
+    $scope.currentCategory = category;
+    $scope.loaded = false;
+    FlashCardsFactory.getFlashCards(category).then(function (data)
+        {
+          $scope.loaded = true;
+          $scope.flashCards = data;
+        });
+  }
+
+  $scope.answerQuestion = function (answer, flashCard)
+  {
+    if (!flashCard.answered) {
+      flashCard.answered = true;
+      flashCard.answeredCorrectly = answer.correct;
+      if (answer.correct) ScoreFactory.correct++;
+      else ScoreFactory.incorrect++;
+    }
+  }
+});
